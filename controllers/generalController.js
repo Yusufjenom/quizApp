1e46cdb15375e7d0f2d57a811e58c5fda29e8ce8
Yusufjenom <jenomy32@gmail.com>
2023-12-20 05:01:42 -0800

    [backend]build: general controller that will be used by both type of users

---
+++ controllers/generalController.js
@@ -0,0 +1,14 @@
+
+
+
+
+const getLandingPage = async (req, res) => {
+    // try{
+    //    res.status(200).render("landingPage")
+    // }
+    // catch(err){
+    //     console.log(err.message);
+    // }
+};
+
+module.exports = {getLandingPage};
