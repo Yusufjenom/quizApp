c5d5d94bd582040cc0b73746e159ca5908485bec
Yusufjenom <jenomy32@gmail.com>
2023-12-20 15:38:59 -0800

    [backend]build: added the route responsible for displaying the upload Q and A component

--- routers/adminRoutes.js
+++ routers/adminRoutes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const {uploadQuestionsAndAnswers} = require('../controllers/adminController');
+const {uploadQuestionsAndAnswers, getQA} = require('../controllers/adminController');
 
 
 //QUESTION AND ANSWER UPLOAD ROUTE
 router.post('/upload-questions-answers', uploadQuestionsAndAnswers);
+
+//GET FORM TO UPLOAD BOTH QUESTIONS AND ANSWER
+router.get('/upload-questions-answers', getQA);
 
 
 module.exports = router;
