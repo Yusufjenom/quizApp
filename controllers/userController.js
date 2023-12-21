7adab06849266b19e2d4f7c32042ca1df7b8b09d
Yusufjenom <jenomy32@gmail.com>
2023-12-20 17:22:15 -0800

    [Backend]refactor: modified the getQuestions controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,15 @@
+const {QuestionAndAnswerModel} = require('../models/Q & A/q&aModel');
 
 
 
 //DISPLAY QUESTIONS
 const getQuestions = async (req, res) => {
     try{
-     res.status(200).render("viewQuestions");
+      const questions = await QuestionAndAnswerModel.find();
+      console.log(questions[0].qAnda);
+      const displayQuestions = questions[0].qAnda[0];
+      console.log(displayQuestions);
+     res.status(200).render("viewQuestions", {displayQuestions});
     }
     catch(err){
         console.log(err.message);
