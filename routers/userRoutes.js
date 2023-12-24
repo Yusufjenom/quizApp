b31a249359e8c0f2ad97e6399adffc06e34e3ac2
Yusufjenom <jenomy32@gmail.com>
2023-12-24 07:35:19 -0800

    [Backend]build: created both the signup and login routes

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const {getQuestions, submitAnswers} = require('../controllers/userController');
+const {getQuestions,
+        submitAnswers,
+        createUser,
+        loginUser
+     } = require('../controllers/userController');
 
 router.get('/questions', getQuestions);
 
 router.post('/answers', submitAnswers);
+
+router.post('/signup-user', createUser);
+
+router.post('/login-user', loginUser);
 
 module.exports = router;
