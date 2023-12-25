7eae81831fae54c753e4968bc00bfed7248e099c
Yusufjenom <jenomy32@gmail.com>
2023-12-25 08:16:36 -0800

    [Backend]build: created the controller to fetch course by id and also a contoller to select a particular subject

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const { QuestionAndAnswerModel } = require('../models/Q & A/q&aModel');
 const { UserModel } = require('../models/users/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {errorHandler} = require('../utils/errors/errorHandler');
+const { errorHandler } = require('../utils/errors/errorHandler');
 
 
 const period = 60 * 60 * 24;
@@ -93,25 +93,25 @@ const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await UserModel.findOne({ email });
-        
+
         if (user) {
-            const isPassword = await bcrypt.compare(password, user.password);       
+            const isPassword = await bcrypt.compare(password, user.password);
             if (isPassword) {
-              await jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: period },
-                 async (err, token) => {
-                    if (token) {
-                        console.log("logged")
-                        res.cookie("userToken", token);
-                        res.status(200).json({
-                            success: true,
-                            user,
-                        })
-                    }
-                    else {
-                        throw new Error(err.message)
-                    }
-              })
-                
+                await jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: period },
+                    async (err, token) => {
+                        if (token) {
+                            console.log("logged")
+                            res.cookie("userToken", token);
+                            res.status(200).json({
+                                success: true,
+                                user,
+                            })
+                        }
+                        else {
+                            throw new Error(err.message)
+                        }
+                    })
+
             }
             else {
                 throw new Error("incorrect password");
@@ -150,11 +150,38 @@ const getUserLoginForm = async (req, res) => {
     }
 };
 
+const getCoursesList = async (req, res) => {
+    try {
+        const questions = await QuestionAndAnswerModel.find();
+        res.status(200).render("listOfCourses", {questions});
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+};
+
+const getACourseByCourseId = async (req, res) => {
+    try{
+      const {id} = req.params;
+      console.log(id)
+      const course = await QuestionAndAnswerModel.findById(id);
+      console.log(course)
+      const displayQuestions = course.qAnda[0];
+      //console.log(displayQuestions);
+      res.status(200).render("viewQuestions", { displayQuestions });
+    }
+    catch(err){
+        console.log(err.message)
+    }
+};
+
 module.exports = {
     getQuestions,
     submitAnswers,
     createUser,
     loginUser,
     getUserSignUpForm,
-    getUserLoginForm
+    getUserLoginForm,
+    getCoursesList,
+    getACourseByCourseId
 };
