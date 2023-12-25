9e69b8450eb51e5a804c8e66fd959540bf7bf5a8
Yusufjenom <jenomy32@gmail.com>
2023-12-24 19:02:06 -0800

    [Backend]build: created the create and login admin post controllers for authentication

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const { QuestionAndAnswerModel } = require('../models/Q & A/q&aModel');
 const {AdminModel} = require('../models/admin/adminModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const {errorHandler} = require('../utils/errors/errorHandler');
 
 const period = 60 * 60 * 24;
 //GET QUESTIONS AND ANSWER UPLOAD FORM
@@ -34,23 +35,35 @@ const uploadQuestionsAndAnswers = async (req, res) => {
 const createAdmin = async (req, res) => {
     try{
       const {email, password, name} = req.body;
+      const isRegistered = await AdminModel.findOne({email});
 
-      const salt = await bcrypt.genSalt(10)
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const newAdmin = new AdminModel({
-        email,
-        name,
-        password: hashedPassword
-      });
-      const savedAdmin = await newAdmin.save();
-      res.status(201).json({
-        success: true,
-        savedAdmin
-      })
-
+      if(!isRegistered){
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newAdmin = new AdminModel({
+          email,
+          name,
+          password: hashedPassword
+        });
+        const savedAdmin = await newAdmin.save();
+        res.status(201).json({
+          success: true,
+          savedAdmin
+        })
+  
+      }else{
+        throw new Error("this email is already registered");
+      }
+      
     }
     catch(err){
-        console.log(err.message);
+        //console.log(err.message);
+        const error = errorHandler(err);
+        console.log(error);
+        return res.status(400).json({
+            success: false,
+            error
+        });
     }
 };
 
@@ -85,10 +98,12 @@ const loginAdmin = async (req, res) => {
      }
     }
     catch(err){
-        console.log(err.message)
+        //console.log(err.message)
+        const error = errorHandler(err);
+        console.log(error)
         res.status(400).json({
             success: false,
-            message: err.message
+            error
         })
     }
 };
