ce5cabc87f0ed5653caece853fd3f0b8c6e2bed2
Yusufjenom <jenomy32@gmail.com>
2023-12-26 13:24:05 -0800

    [Backend]build: created the admin dashboard controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,16 +1,16 @@
 const { QuestionAndAnswerModel } = require('../models/Q & A/q&aModel');
-const {AdminModel} = require('../models/admin/adminModel');
+const { AdminModel } = require('../models/admin/adminModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {errorHandler} = require('../utils/errors/errorHandler');
+const { errorHandler } = require('../utils/errors/errorHandler');
 
 const period = 60 * 60 * 24;
 //GET QUESTIONS AND ANSWER UPLOAD FORM
 const getQA = async (req, res) => {
-    try{
-    res.status(200).render("uploadQuestionsAndAnswers.ejs");
+    try {
+        res.status(200).render("uploadQuestionsAndAnswers.ejs");
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
     }
 };
@@ -20,10 +20,10 @@ const uploadQuestionsAndAnswers = async (req, res) => {
     try {
         const data = req.body;
         const newUpload = new QuestionAndAnswerModel({
-            qAnda : data
+            qAnda: data
         });
         const savedQuestionsAndAnswer = await newUpload.save();
-        console.log(savedQuestionsAndAnswer);
+        //console.log(savedQuestionsAndAnswer);
         res.status(201).redirect("/");
     }
     catch (err) {
@@ -33,30 +33,30 @@ const uploadQuestionsAndAnswers = async (req, res) => {
 
 
 const createAdmin = async (req, res) => {
-    try{
-      const {email, password, name} = req.body;
-      const isRegistered = await AdminModel.findOne({email});
+    try {
+        const { email, password, name } = req.body;
+        const isRegistered = await AdminModel.findOne({ email });
 
-      if(!isRegistered){
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const newAdmin = new AdminModel({
-          email,
-          name,
-          password: hashedPassword
-        });
-        const savedAdmin = await newAdmin.save();
-        res.status(201).json({
-          success: true,
-          savedAdmin
-        })
-  
-      }else{
-        throw new Error("this email is already registered");
-      }
-      
+        if (!isRegistered) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const newAdmin = new AdminModel({
+                email,
+                name,
+                password: hashedPassword
+            });
+            const savedAdmin = await newAdmin.save();
+            res.status(201).json({
+                success: true,
+                savedAdmin
+            })
+
+        } else {
+            throw new Error("this email is already registered");
+        }
+
     }
-    catch(err){
+    catch (err) {
         //console.log(err.message);
         const error = errorHandler(err);
         console.log(error);
@@ -69,67 +69,78 @@ const createAdmin = async (req, res) => {
 
 
 const loginAdmin = async (req, res) => {
-    try{
-     const {email, password} = req.body;
-     const user = await AdminModel.findOne({email});
+    try {
+        const { email, password } = req.body;
+        const user = await AdminModel.findOne({ email });
 
-     if(user){
-       const isPassword = await bcrypt.compare(password, user.password);
+        if (user) {
+            const isPassword = await bcrypt.compare(password, user.password);
 
-       if(isPassword){
-        const token = await jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: period});
-        if(token){
-            res.cookie("adminToken", token);
-            res.status(200).json({
-                success: true,
-                user,
-            })
+            if (isPassword) {
+                const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: period });
+                if (token) {
+                    res.cookie("adminToken", token);
+                    res.status(200).json({
+                        success: true,
+                        user,
+                    })
+                }
+                else {
+                    throw new Error("invalid token");
+                }
+            }
+            else {
+                throw new Error("incorrect password");
+            }
         }
-        else{
-            throw new Error("invalid token")
+        else {
+            throw new Error("invalid email address");
         }
-       }
-       else{
-        throw new Error("incorrect password");
-       }
-     }
-     else{
-        throw new Error("invalid email address");
-     }
     }
-    catch(err){
+    catch (err) {
         //console.log(err.message)
         const error = errorHandler(err);
-        console.log(error)
+        console.log(error);
         res.status(400).json({
             success: false,
             error
-        })
-    }
+        });
+    };
 };
 
 const getAdminSignupForm = async (req, res) => {
-    try{
-   res.status(200).render("signupAdmin");
+    try {
+        res.status(200).render("signupAdmin");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 };
 
 const getAdminLoginForm = async (req, res) => {
-    try{
-   res.status(200).render("loginAmin");
+    try {
+        res.status(200).render("loginAmin");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
-}
+};
 
-module.exports = {uploadQuestionsAndAnswers,
-                    getQA,
-                    createAdmin, 
-                    loginAdmin,
-                    getAdminSignupForm,
-                    getAdminLoginForm
-                };
+const adminDashboard = async (req, res) => {
+    try {
+        res.status(200).render("adminDashboard");
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+};
+
+module.exports = {
+    uploadQuestionsAndAnswers,
+    getQA,
+    createAdmin,
+    loginAdmin,
+    getAdminSignupForm,
+    getAdminLoginForm,
+    adminDashboard
+};
