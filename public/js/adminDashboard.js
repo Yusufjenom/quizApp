62786e106d7f753004a7dd9c198e26d74a9a026a
Yusufjenom <jenomy32@gmail.com>
2023-12-26 13:31:07 -0800

    [Frontend]build: admin dashboard js file for styling dynamic displays

---
+++ public/js/adminDashboard.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('performanceGraph').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Math', 'Science', 'History', 'Literature'],
+            datasets: [{
+                label: 'Average Score',
+                data: [85, 92, 78, 88],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                    'rgba(255, 206, 86, 0.7)',
+                    'rgba(75, 192, 192, 0.7)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
