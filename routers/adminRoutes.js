f21116943c5d6db579263855ddfd6541860bcec9
Yusufjenom <jenomy32@gmail.com>
2023-12-27 15:40:39 -0800

    [Backend]refactor: modified the logout admin get route

--- routers/adminRoutes.js
+++ routers/adminRoutes.js
@@ -1,19 +1,23 @@
 const router = require('express').Router();
+const {verifyAdmin} = require('../middlewares/adminAuth');
+const {setCache} = require('../middlewares/cache-control');
 const {uploadQuestionsAndAnswers, 
             getQA, 
             createAdmin,
             loginAdmin,
             getAdminSignupForm,
             getAdminLoginForm,
-            adminDashboard
+            adminDashboard,
+            getAllUsers,
+            logoutAdmin
         } = require('../controllers/adminController');
 
 
 //QUESTION AND ANSWER UPLOAD ROUTE
-router.post('/upload-questions-answers', uploadQuestionsAndAnswers);
+router.post('/upload-questions-answers',verifyAdmin, uploadQuestionsAndAnswers);
 
 //GET FORM TO UPLOAD BOTH QUESTIONS AND ANSWER
-router.get('/upload-questions-answers', getQA);
+router.get('/upload-questions-answers',verifyAdmin, getQA);
 
 //CREATE AN ADMIN
 router.post('/signup-admin', createAdmin);
@@ -25,7 +29,11 @@ router.get('/signup-admin', getAdminSignupForm);
 
 router.get('/login-admin', getAdminLoginForm);
 
-router.get('/admin-dashboard', adminDashboard);
+router.get('/admin-dashboard',verifyAdmin, setCache, adminDashboard);
+
+router.get('/users', verifyAdmin, setCache, getAllUsers);
+
+router.get('/logout-admin', logoutAdmin);
 
 
 module.exports = router;
