552de1ceefc5ca6489f9e74801a05503c1ef9206
Yusufjenom <jenomy32@gmail.com>
2023-12-28 11:47:30 -0800

    [Backend]refactor: installed validator to handle email validation errors and also created a function with Regex to handle password validation with special characters

--- models/admin/adminModel.js
+++ models/admin/adminModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-//const {isEmail} = require('validator');
+const {isEmail} = require('validator');
 
+const checkForSpecialCharacters = (value) => {
+    let specialCharacterWithRegex = /[!@#$%^&*(),.{}|<>]/;
+    return specialCharacterWithRegex.test(value);
+}
 const adminSchema = new mongoose.Schema({
     name:{
         type: String,
@@ -9,12 +13,18 @@ const adminSchema = new mongoose.Schema({
     email:{
         type: String,
         required: [true, "kindly enter an email address"],
-        unique: true
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, "Please enter a valid email"]
     },
     password:{
         type: String,
         required: [true, "kindly enter a password"],
-        minlength: 6
+        minlength: [6, 'Minimum password length is 6 characters'],
+        validate: {
+            validator: checkForSpecialCharacters,
+            message: "Password must contain a special character/symbol"
+        }
     }
 }, {timestamps: true});
 
